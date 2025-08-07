4fa9517e7a1938a3a39c146bbe4464c379f0725f
Isaiah Ojo <51814402+LafyOjo@users.noreply.github.com>
2025-08-07 01:01:55 +0100

    new beginning

--- dashboard/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders dashboard text', () => {
-  render(<App />);
-  const element = screen.getByText(/hello, dashboard/i);
-  expect(element).toBeInTheDocument();
-});
